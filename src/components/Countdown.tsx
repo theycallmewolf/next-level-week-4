eaa9f3fc5e84fb1155e590d080c0dc10c1c12335
bruno-wolf <bruno.m.lobato@gmail.com>
2021-02-24 00:48:36 +0000

    adds seconds countdown

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,21 +1,48 @@
+import { useCallback, useState, useEffect } from 'react';
 import styles from '../styles/components/Countdown.module.css';
 
 export default function Countdown() {
+  const [time, setTime] = useState(23 * 60);
+  const [active, setActive] = useState(false);
+
+  const minutes =  Math.floor(time / 60);
+  const seconds = time % 60;
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+  const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+  
+  const startCountDown = useCallback(() => {
+    setActive(true);
+  }, []);
+  
+  useEffect(() =>{
+    if(active && time > 0) {
+      setTimeout(() =>{
+        setTime(time -1);
+      }, 1000);
+    }
+  }, [active, time]);
+
   return (
     <div>
       <div className={styles.countdownContainer}>
         <div>
-          <span>2</span>
-          <span>5</span>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
         </div>
         <span>:</span>
         <div>
-          <span>0</span>
-          <span>0</span>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
         </div>
       </div>
 
-      <button type="button" className={styles.countdownButton}>Começar</button>
+      <button 
+        type="button"
+        onClick={startCountDown}
+        className={styles.countdownButton}>
+          Começar
+        </button>
     </div>
   )
 }
