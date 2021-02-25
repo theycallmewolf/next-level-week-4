0f2e1f54a22003f0bcee221fe0a5d333c83ed2d4
bruno-wolf <bruno.m.lobato@gmail.com>
2021-02-25 23:21:25 +0000

    update profile component with dinamic level

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../hooks/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export default function Profile() {
+
+  const { level } = useContext(ChallengesContext);
   return (
     <div className={styles.profileContainer}>
       <img src="https://avatars.githubusercontent.com/u/57861255?s=460&u=fab67d1be099c5eebb9bcd5d51c95bd86070f8b4&v=4" alt="Wolf"/>
@@ -8,7 +12,7 @@ export default function Profile() {
         <strong>They Call Me Wolf</strong>
         <p>
           <img src="icons/level.svg" alt="level"/>
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
